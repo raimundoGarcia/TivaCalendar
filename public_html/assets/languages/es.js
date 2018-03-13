a9e8cbcc573f499f599a85f5d464bdd0e0339d29
informatica3 <informatica3@informatica3-pc.consultia.dom>
2018-03-13 12:52:10 +0100

    pruebas 1.0

---
+++ public_html/assets/languages/es.js
@@ -0,0 +1,22 @@
+// Day, Month
+var wordMonth = new Array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
+
+var wordDay_mon = "Lunes";
+var wordDay_tue = "Martes";
+var wordDay_wed = "Miercoles";
+var wordDay_thu = "Jueves";
+var wordDay_fri = "Viernes";
+var wordDay_sat = "Sabado";
+var wordDay_sun = "Domingo";
+
+// View Button
+var calendar_view = "Calendario"
+var list_view = "Lista"
+var back = "Volver"
+
+// Calendar Button
+var prev_year = "Año anterior"
+var prev_month = "Mes anterior"
+var next_month = "Siguente mes"
+var next_year = "Siguiente año"
+
